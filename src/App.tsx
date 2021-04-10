b9661a05335e3665baa2e9c4111d95f84b86a16d
Darlan Tódero ten Caten <darlan.caten@simplyas.com>
2021-04-10 20:32:20 -0300

    feat: added AnimalsListPageViewModel

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-const App = () => {
+import { HttpService } from "./services/HttpService";
+import { UseCasesContext } from "./useCases/UseCasesContext";
+import RequestAnimalsUseCase from "./useCases/RequestAnimalsUseCase";
+import RequestAnimalPhotoUseCase from "./useCases/RequestAnimalPhotoUseCase";
+
+import AnimalsListPageViewModel from "./presentation/AnimalsListPage/AnimalsListPageViewModel";
+
+const App = ({ httpService }: { httpService: HttpService }) => {
   return (
-    <div>
-      <h1>Shelter Buddy</h1>
-    </div>
+    <UseCasesContext.Provider
+      value={{
+        requestAnimalsUseCase: new RequestAnimalsUseCase(httpService),
+        requestAnimalPhotoUseCase: new RequestAnimalPhotoUseCase(httpService),
+      }}
+    >
+      <AnimalsListPageViewModel />
+    </UseCasesContext.Provider>
   );
 };
 
