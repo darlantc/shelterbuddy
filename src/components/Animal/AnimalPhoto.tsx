131a8f0a1790304e2893e1d5f0a7fd4c5c803728
Darlan Tódero ten Caten <darlan.caten@simplyas.com>
2021-04-11 16:40:43 -0300

    fix: AnimalPhoto don't update state if not mounted anymore

--- src/components/Animal/AnimalPhoto.tsx
+++ src/components/Animal/AnimalPhoto.tsx
@@ -3,15 +3,30 @@ import useIsFetchingPromise from "../../hooks/useIsFetchingPromise";
 import { useUseCasesContext } from "../../useCases/UseCasesContext";
 
 import PlaceholderImage from "../../assets/images/placeholder.png";
-import useDidMount from "../../hooks/useDidMount";
+import { useState, useEffect } from "react";
 
 const AnimalPhoto = ({ id, alt }: { id: number; alt: string }) => {
+  const [isMounted, setIsMounted] = useState(false);
+
   const { requestAnimalPhotoUseCase } = useUseCasesContext()!;
   const [isFetching, photo, refresh] = useIsFetchingPromise<AnimalPhotoEntity>(
     async () => requestAnimalPhotoUseCase.requestAnimalPhoto(id)
   );
 
-  useDidMount(refresh);
+  useEffect(() => {
+    setIsMounted(true);
+
+    refresh();
+
+    return () => {
+      setIsMounted(false);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (!isMounted) {
+    return <div className="thumbnail-photo" />;
+  }
 
   let image = PlaceholderImage;
   if (!isFetching && photo) {
