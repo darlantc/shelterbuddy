04394c07c514d814b9b1e4587ae3da907e5c0ca2
Darlan Tódero ten Caten <darlan.caten@simplyas.com>
2021-04-11 14:29:07 -0300

    feat: finished implementation of AnimalPhoto component

--- src/components/Animal/AnimalPhoto.tsx
+++ src/components/Animal/AnimalPhoto.tsx
@@ -1,11 +1,25 @@
+import AnimalPhotoEntity from "../../entities/AnimalPhotoEntity";
+import useIsFetchingPromise from "../../hooks/useIsFetchingPromise";
+import { useUseCasesContext } from "../../useCases/UseCasesContext";
+
+import PlaceholderImage from "../../assets/images/placeholder.png";
+import useDidMount from "../../hooks/useDidMount";
+
 const AnimalPhoto = ({ id, alt }: { id: number; alt: string }) => {
-  return (
-    <img
-      src="https://fakeimg.pl/250x250/"
-      alt={alt}
-      className="thumbnail-photo"
-    />
+  const { requestAnimalPhotoUseCase } = useUseCasesContext()!;
+  const [isFetching, photo, refresh] = useIsFetchingPromise<AnimalPhotoEntity>(
+    async () => requestAnimalPhotoUseCase.requestAnimalPhoto(id)
   );
+
+  useDidMount(refresh);
+
+  let image = PlaceholderImage;
+  if (!isFetching && photo) {
+    const baseUrl = "https://shelterbuddy-us-uat.shelterbuddy.io/";
+    image = `${baseUrl}${photo.thumbnail}`;
+  }
+
+  return <img src={image} alt={alt} className="thumbnail-photo" />;
 };
 
 export default AnimalPhoto;
