d8cd511d1fa91b2a58ddd7522730d9f6bc158fa0
Darlan Tódero ten Caten <darlan.caten@simplyas.com>
2021-04-11 15:12:54 -0300

    feat: allow user to sort animals by table columns on Desktop

--- src/presentation/AnimalsListPage/AnimalsListAsTable.tsx
+++ src/presentation/AnimalsListPage/AnimalsListAsTable.tsx
@@ -1,10 +1,12 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
+
 import Table from "../../components/Table/Table";
 import AnimalPhoto from "../../components/Animal/AnimalPhoto";
 import FlatButton from "../../components/Button/FlatButton";
 import { ChevronRightIcon } from "../../components/Icons/Icons";
 
 import AnimalEntity, { AnimalProps } from "../../entities/AnimalEntity";
+import { dynamicSortByProperty } from "../../utils/sortUtils";
 
 interface TableRowInterface extends AnimalProps {
   photo: ReactElement;
@@ -20,21 +22,37 @@ const AnimalsListAsTable: React.FC<AnimalsListAsTableProps> = ({
   animalsList,
   didWantToSeeDetails,
 }) => {
+  const [orderBy, setOrderBy] = useState("name");
+  const [order, setOrder] = useState<"asc" | "desc">("asc");
+
+  const onRequestSort = (
+    event: React.MouseEvent<unknown>,
+    property: string
+  ) => {
+    setOrderBy(property);
+    setOrder(order === "asc" ? "desc" : "asc");
+  };
+
+  animalsList.sort(dynamicSortByProperty(orderBy, order === "asc"));
+
   return (
     <Table<TableRowInterface>
+      onRequestSort={onRequestSort}
+      orderBy={orderBy}
+      order={order}
       columns={[
         { id: "photo" },
-        { id: "name", label: "Name" },
-        { id: "type", label: "Type" },
-        { id: "breed", label: "Breed" },
-        { id: "gender", label: "Gender" },
-        { id: "color", label: "Color" },
+        { id: "name", label: "Name", isSortable: true },
+        { id: "type", label: "Type", isSortable: true },
+        { id: "breed", label: "Breed", isSortable: true },
+        { id: "gender", label: "Gender", isSortable: true },
+        { id: "color", label: "Color", isSortable: true },
         { id: "action" },
       ]}
       rows={animalsList.map((animal) => {
         const name = animal.name || "-";
         return {
-          id: animal.id,
+          id: `${animal.id}`,
           data: {
             photo: (
               <AnimalPhoto id={animal.id} alt={animal.name || animal.type} />
