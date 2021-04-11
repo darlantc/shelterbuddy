e3dd9a74270503436dbc824200cff43b61d80874
Darlan Tódero ten Caten <darlan.caten@simplyas.com>
2021-04-11 04:03:18 -0300

    feat: enhanced Table styles

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,16 +1,46 @@
+import { ReactElement } from "react";
+
 import { makeStyles } from "@material-ui/core/styles";
 import MaterialTable from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
-import { ReactElement } from "react";
+import {
+  darkGrayColor,
+  lightGrayColor,
+  blackColor,
+} from "../../assets/styles/styles";
 
 const useStyles = makeStyles({
   table: {
     minWidth: 650,
+    borderCollapse: "separate",
+    borderSpacing: "0px 12px",
+  },
+  headerCell: {
+    fontWeight: 500,
+    color: darkGrayColor,
+    fontSize: 16,
+    border: "none",
+  },
+  tableRow: {
+    backgroundColor: lightGrayColor,
+  },
+  rowCell: {
+    fontWeight: 500,
+    color: blackColor,
+    fontSize: 16,
+    border: "none",
+    padding: 12,
+    "&:first-child": {
+      borderTopLeftRadius: 12,
+      borderBottomLeftRadius: 12,
+    },
+    "&:last-child": {
+      borderTopRightRadius: 12,
+      borderBottomRightRadius: 12,
+    },
   },
 });
 
@@ -34,32 +64,33 @@ const Table = <T,>({ columns, rows }: TableProps<T>) => {
   const classes = useStyles();
 
   return (
-    <TableContainer component={Paper}>
-      <MaterialTable className={classes.table} aria-label="table">
-        <TableHead>
-          <TableRow>
+    <MaterialTable className={classes.table} aria-label="table">
+      <TableHead>
+        <TableRow>
+          {columns.map((column) => (
+            <TableCell key={column.id} classes={{ root: classes.headerCell }}>
+              {column.label}
+            </TableCell>
+          ))}
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {rows.map((row) => (
+          <TableRow key={row.id} classes={{ root: classes.tableRow }}>
             {columns.map((column) => (
-              <TableCell key={column.id}>{column.label}</TableCell>
+              <TableCell
+                key={`${row.id}_${column.id}`}
+                align={column.align || "left"}
+                classes={{ root: classes.rowCell }}
+              >
+                {/* @ts-ignore */}
+                {row.data[column.id] || "-"}
+              </TableCell>
             ))}
           </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              {columns.map((column) => (
-                <TableCell
-                  key={`${row.id}_${column.id}`}
-                  align={column.align || "left"}
-                >
-                  {/* @ts-ignore */}
-                  {row.data[column.id] || "-"}
-                </TableCell>
-              ))}
-            </TableRow>
-          ))}
-        </TableBody>
-      </MaterialTable>
-    </TableContainer>
+        ))}
+      </TableBody>
+    </MaterialTable>
   );
 };
 
