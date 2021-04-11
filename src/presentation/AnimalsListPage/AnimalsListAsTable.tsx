9beae3959ac4a7b23182aa7b8d405c2dfb82bff1
Darlan Tódero ten Caten <darlan.caten@simplyas.com>
2021-04-11 10:39:10 -0300

    refactor: moved animals listing in table to its own component

---
+++ src/presentation/AnimalsListPage/AnimalsListAsTable.tsx
@@ -0,0 +1,60 @@
+import { ReactElement } from "react";
+import Table from "../../components/Table/Table";
+import AnimalPhoto from "../../components/Animal/AnimalPhoto";
+import FlatButton from "../../components/Button/FlatButton";
+
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import AnimalEntity, { AnimalProps } from "../../entities/AnimalEntity";
+
+interface TableRowInterface extends AnimalProps {
+  photo: ReactElement;
+  name: string;
+  action: ReactElement;
+}
+
+interface AnimalsListAsTableProps {
+  animalsList: AnimalEntity[];
+  didWantToSeeDetails: (id: number) => () => void;
+}
+const AnimalsListAsTable: React.FC<AnimalsListAsTableProps> = ({
+  animalsList,
+  didWantToSeeDetails,
+}) => {
+  return (
+    <Table<TableRowInterface>
+      columns={[
+        { id: "photo" },
+        { id: "name", label: "Name" },
+        { id: "type", label: "Type" },
+        { id: "breed", label: "Breed" },
+        { id: "gender", label: "Gender" },
+        { id: "color", label: "Color" },
+        { id: "action" },
+      ]}
+      rows={animalsList.map((animal) => {
+        const name = animal.name || "-";
+        return {
+          id: animal.id,
+          data: {
+            photo: (
+              <AnimalPhoto id={animal.id} alt={animal.name || animal.type} />
+            ),
+            id: animal.id,
+            name,
+            type: animal.type,
+            breed: animal.breed,
+            gender: animal.gender,
+            color: animal.color,
+            action: (
+              <FlatButton onClick={didWantToSeeDetails(animal.id)}>
+                Details <ChevronRightIcon />
+              </FlatButton>
+            ),
+          },
+        };
+      })}
+    />
+  );
+};
+
+export default AnimalsListAsTable;
--- src/presentation/AnimalsListPage/AnimalsListPageView.tsx
+++ src/presentation/AnimalsListPage/AnimalsListPageView.tsx
@@ -1,20 +1,11 @@
-import { ReactElement } from "react";
+import { Hidden } from "@material-ui/core";
 
 import { GridContainer, GridItem } from "../../components/Grid";
 import { Card } from "../../components/Card";
 import SearchBar from "../../components/SearchBar/SearchBar";
-import Table from "../../components/Table/Table";
-import AnimalPhoto from "../../components/Animal/AnimalPhoto";
-import FlatButton from "../../components/Button/FlatButton";
+import AnimalsListAsTable from "./AnimalsListAsTable";
 
-import ChevronRightIcon from "@material-ui/icons/ChevronRight";
-
-import AnimalEntity, { AnimalProps } from "../../entities/AnimalEntity";
-interface TableRowInterface extends AnimalProps {
-  photo: ReactElement;
-  name: string;
-  action: ReactElement;
-}
+import AnimalEntity from "../../entities/AnimalEntity";
 
 const AnimalsListPageView = ({
   isFetching,
@@ -57,42 +48,14 @@ const AnimalsListPageView = ({
             </GridItem>
           </GridContainer>
           <GridContainer>
-            <Table<TableRowInterface>
-              columns={[
-                { id: "photo" },
-                { id: "name", label: "Name" },
-                { id: "type", label: "Type" },
-                { id: "breed", label: "Breed" },
-                { id: "gender", label: "Gender" },
-                { id: "color", label: "Color" },
-                { id: "action" },
-              ]}
-              rows={filteredAnimalsList.map((animal) => {
-                const name = animal.name || "-";
-                return {
-                  id: animal.id,
-                  data: {
-                    photo: (
-                      <AnimalPhoto
-                        id={animal.id}
-                        alt={animal.name || animal.type}
-                      />
-                    ),
-                    id: animal.id,
-                    name,
-                    type: animal.type,
-                    breed: animal.breed,
-                    gender: animal.gender,
-                    color: animal.color,
-                    action: (
-                      <FlatButton onClick={didWantToSeeDetails(animal.id)}>
-                        Details <ChevronRightIcon />
-                      </FlatButton>
-                    ),
-                  },
-                };
-              })}
-            ></Table>
+            <GridItem xs>
+              <Hidden smDown>
+                <AnimalsListAsTable
+                  animalsList={filteredAnimalsList}
+                  didWantToSeeDetails={didWantToSeeDetails}
+                />
+              </Hidden>
+            </GridItem>
           </GridContainer>
         </Card>
       </GridItem>
