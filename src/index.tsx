655ef4d0ce599b2980210aea4afb8cc6df2cf3de
Darlan Tódero ten Caten <darlan.caten@simplyas.com>
2022-05-25 16:38:27 -0300

    fix: using correct server URL

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./assets/styles/index.css";
 import App from "./App";
+import "./assets/styles/index.css";
 import reportWebVitals from "./reportWebVitals";
-import { HttpService } from "./services/httpService/HttpService";
 import AxiosHttpService from "./services/httpService/AxiosHttpService";
+import { HttpService } from "./services/httpService/HttpService";
 import ShelterBuddyService from "./services/ShelterBuddyService";
 
 const httpService: HttpService = new AxiosHttpService(
-  // "https://shelterbuddy-us-uat.shelterbuddy.io"
-  "http://localhost:3000"
+    "https://shelterbuddy.vercel.app"
 );
 
 const sbService: ShelterBuddyService = new ShelterBuddyService(httpService);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App sbService={sbService} />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <App sbService={sbService} />
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 reportWebVitals();
